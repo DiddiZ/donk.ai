edd390a9638e9893cc0019e9fc651cbef6e6681b
Robin Kupper <robin.kupper@rwth-aachen.de>
2021-07-29 16:37:05 +0200

    Code to work with.

---
+++ donk/dynamics/prior/prior_nn.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn.mixture import GaussianMixture
+
+from donk.dynamics.prior import DynamicsPrior
+
+
+class NNPrior(DynamicsPrior):
+
+    def __init__(self, model) -> None:
+        self.model = model
+
+    def update(self, XUX):
+        """Updates the prior.
+
+        Args:
+            XUX: Transitions with shape (N, dX+dU+dX)
+        """
+        self.model.fit(XUX)  # TODO does not work this way
+
+        # Enable warm start fpr further updates
+        self.gmm.warm_start = True
+
+    def eval(self, XUX):
+        """Evaluate prior.
+
+        Args:
+            XUX: Transitions with shape (N, dX+dU+dX)
+
+        Returns:
+            mu0, Phi, m, n0
+        """
+        wts = np.mean(self.gmm.predict_proba(XUX), axis=0)
+
+        mu0 = np.sum(wts[:, np.newaxis] * self.gmm.means_, axis=0)
+        Phi = np.sum(wts[:, np.newaxis, np.newaxis] * self.gmm.covariances_, axis=0)
+
+        # Use 1 for m and n0 instead of correct values as suggest by Fu et al. to compensate for discrepancies in
+        # population size between prior and sampling.
+        # see: https://arxiv.org/abs/1509.06841
+        m = n0 = 1
+
+        return mu0, Phi, m, n0
