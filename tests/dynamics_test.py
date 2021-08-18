3aa1944e5e26135b3eec52565af7704b7a0c4cc0
Robin Kupper <robin.kupper@rwth-aachen.de>
2021-08-18 13:35:35 +0200

    Fixed dynamics with prior unittest.

--- tests/dynamics_test.py
+++ tests/dynamics_test.py
@@ -49,7 +49,11 @@ class Test_LinearDynamics(unittest.TestCase):
                 7.16121964e-03, 9.89241563e-03, 6.08668508e-01
             ]
         )
+        # Check s.p.d.
         assert_array_equal(dyn_covar, np.swapaxes(dyn_covar, 1, 2), "dyn_covar not symmetric")
+        for t in range(T):
+            with self.subTest(t=t):
+                self.assertTrue(all(np.linalg.eigvalsh(dyn_covar[t]) >= -1e-16), f"Negative eigenvalues {np.linalg.eigvalsh(dyn_covar[t])}")
 
     def test_fit_lr_with_prior(self):
         from donk.dynamics.linear_dynamics import fit_lr
@@ -65,7 +69,7 @@ class Test_LinearDynamics(unittest.TestCase):
         prior = GMMPrior(8, random_state=0)
         prior.update(transitions)
 
-        dyn = fit_lr(X[:3], U[:3], regularization=1e-6)
+        dyn = fit_lr(X[:3], U[:3], prior, regularization=1e-6)
         Fm, fv, dyn_covar = dyn.Fm, dyn.fv, dyn.dyn_covar
 
         # Check shapes
@@ -76,18 +80,21 @@ class Test_LinearDynamics(unittest.TestCase):
         # Check some values
         assert_allclose(
             Fm[0, -1], [
-                0.00000000e+00, -1.59097886e-03, -1.55670197e-03, 3.66813669e-03, -5.80934222e-06, 2.65460765e-05, -4.26546524e-06,
-                1.35521681e-01, 0.00000000e+00, 3.62010170e-26, -3.62010170e-26, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00,
-                1.81005085e-26, -5.38810305e-02, 4.05879352e-02
+                1.1901108606, -8.2155541561, -10.8582036973, -1.0970622672, -1.6993010666, 3.8033234782, 8.8718137698, 0.2596884979,
+                1.1901108592, 1.1901108596, 1.1901108597, 1.1901108597, 1.1901108598, 1.1901108598, 1.1901108591, 0.5832087672,
+                -0.1688012448
             ]
         )
         assert_allclose(
             Fm[-1, 0],
             [
-                -0.014928676887479259, -0.0534696701687503, 0.02899836537278755, 0.0008340094783329371, -0.02374431115562987,
-                -0.005679050952136661, 0.003968766254604431, 0.033350408132203094, 0.05123335573315034, 0.03796067826102325,
-                0.04711647692994283, 0.02343822112658693, 0.028090633129104786, 0., -0.017981364898149155, 0.07031489952016969,
-                0.0368524392432764
+                0.0531532012, 0.0126352773, -0.1446274682, -0.0114229692, -0.0562042267, 0.0083028760, 0.0082817474, 0.0031557044,
+                0.0086551089, 0.0240643630, -0.0428469319, 0.0176277952, 0.1582171429, 0.4074299145, 0.0057864226, -0.0009779524,
+                0.5487691228
             ],
         )
+        # Check s.p.d.
         assert_array_equal(dyn_covar, np.swapaxes(dyn_covar, 1, 2), "dyn_covar not symmetric")
+        for t in range(T):
+            with self.subTest(t=t):
+                self.assertTrue(all(np.linalg.eigvalsh(dyn_covar[t]) >= 0), f"Negative eigenvalues {np.linalg.eigvalsh(dyn_covar[t])}")
